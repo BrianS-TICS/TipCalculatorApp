f136d12d89f6bc1d6e23d78ad21904f5b69f6f88
BrianS-TICS <briansanchezaguilar@gmail.com>
2022-08-27 19:30:19 -0600

    update validacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-import { useState } from 'react'
 import Formulario from './components/Formulario';
 
 function App() {
-
-  const [amount, setAmount] = useState(0);
-  const [tipAmount, setTipAmount] = useState(0);
-  const [percentage,setPercentage] = useState(0);
-  const [people, setPeople] = useState(0);
-  
-
   return (
       <>
         <div className='titulo'>
--- src/components/EntradaDatos.jsx
+++ src/components/EntradaDatos.jsx
@@ -1,38 +1,74 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-const EntradaDatos = ({setAmount, setPercentage, setPeople}) => {
+const EntradaDatos = ({ setAmount, setPercentage, setPeople }) => {
+
+    const [error, setError] = useState([]);
 
     function handleAmout(e) {
-        const valor = Number(e.target.value);
-        setAmount(valor);
+        const valor = Number(e.target.value)
+
+        /* Si no existe un error (false) entonces se asigna el valor*/
+        if (valor <= 0) {
+            if (!Object.values(error).includes('Amout')) {
+                setError([...error, 'Amout'])
+            }
+        }
+        else {
+            if (error.length > 0) {
+                setError(error.filter(error => error !== 'Amout'))
+            }
+            setAmount(valor)
+        }
     }
 
     function handlePeople(e) {
-        setPeople(Number(e.target.value));
+        const valor = Number(e.target.value)
+        if (valor <= 0) {
+            if (!Object.values(error).includes('People')) {
+                setError([...error, 'People'])
+            }
+        }
+        else {
+            if (error.length > 0) {
+                setError(error.filter(error => error !== 'People'))
+            }
+            setPeople(valor)
+        }
     }
 
     function handlePorcentaje(e) {
-        setPercentage(Number(e.target.value));
+        const valor = Number(e.target.value)
+        if (valor <= 0) {
+            if (!Object.values(error).includes('Porcentaje')) {
+                setError([...error, 'Porcentaje'])
+            }
+        }
+        else {
+            if (error.length > 0) {
+                setError(error.filter(error => error !== 'Porcentaje'))
+            }
+            setPercentage(valor)
+        }
     }
 
     return (
         <div className='entrada-datos'>
             <label htmlFor="bill">Bill</label>
-                <input min={1} max={99999} onChange={handleAmout} id='amount' type="number" placeholder='0' name='bill'/>
+            <input onChange={handleAmout} className={Object.values(error).includes('Amout') ? 'inputError' : ''} id='amount' type="number" placeholder='0' name='bill' />
 
-                <label htmlFor="">Select Tip %</label>
+            <label htmlFor="">Select Tip %</label>
 
-                <div className='propinas'>
-                    <input type="button" id='' className='porcentaje' value='5%'/>
-                    <input type="button" id='' className='porcentaje' value='10%'/>
-                    <input type="button" id='' className='porcentaje' value='15%'/>
-                    <input type="button" id='' className='porcentaje' value='25%'/>
-                    <input type="button" id='' className='porcentaje' value='50%'/>
-                    <input onChange={handlePorcentaje} id='' type="number" className='personalizado' placeholder='Custom'/>
-                </div>
+            <div className='propinas'>
+                <input type="button" id='' className='porcentaje' value='5%' />
+                <input type="button" id='' className='porcentaje' value='10%' />
+                <input type="button" id='' className='porcentaje' value='15%' />
+                <input type="button" id='' className='porcentaje' value='25%' />
+                <input type="button" id='' className='porcentaje' value='50%' />
+                <input onChange={handlePorcentaje} id='' type="number" className={Object.values(error).includes('Porcentaje') ? 'personalizado inputError' : 'personalizado'} placeholder='Custom' />
+            </div>
 
-                <label htmlFor="">Number of people</label>
-                <input onChange={handlePeople} type="number" className='' placeholder='0'/>
+            <label htmlFor="">Number of people</label>
+            <input onChange={handlePeople} type="number" className={Object.values(error).includes('People') ? 'inputError' : ''} placeholder='0' />
         </div>
     )
 }
